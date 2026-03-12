c7bac0bed80ece38c7735ac99d0f838640541176
zhenmeng sun <2159997489@qq.com>
2026-03-12 15:08:18 +0800

    修改部分实体类的文件位置,修改数据库(新数据库文件上传) 操作人:孙哲文

---
+++ it-module-interactive/src/main/java/com/alikeyou/itmoduleinteractive/controller/InteractiveController.java
@@ -0,0 +1,9 @@
+package com.alikeyou.itmoduleinteractive.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/interactive")
+public class InteractiveController {
+}
